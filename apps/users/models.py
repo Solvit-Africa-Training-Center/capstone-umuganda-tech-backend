fbd412529df84cfb335acfdd5e38dd2b82f3f280
Thiedra <mkthierry14@gmail.com>
2025-09-01 12:51:41 +0200

    add column is_verified

--- apps/users/models.py
+++ apps/users/models.py
@@ -19,7 +19,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default= False)
-
+    is_verified= models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()

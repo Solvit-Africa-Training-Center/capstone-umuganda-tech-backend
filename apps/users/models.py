bd8f594dea80d2f78af12a5d243641bd564a9f37
Thiedra <mkthierry14@gmail.com>
2025-09-01 12:38:36 +0200

    add new column is_verified in User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -18,6 +18,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     # Django auth flags
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default= False)
 
     created_at = models.DateTimeField(auto_now_add=True)
 

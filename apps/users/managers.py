54f51d4f11182da3fb30dc7ffe579ed995993f23
Thiedra <mkthierry14@gmail.com>
2025-09-01 12:05:05 +0200

    Creating file of User Manager and managingUser Creation

---
+++ apps/users/managers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import BaseUserManager
+# -------------------------------
+# Custom User Manager
+# -------------------------------
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("Users must have a phone number")
+        user = self.model(phone_number=phone_number, **extra_fields)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()  # For OTP-only accounts
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(phone_number, password, **extra_fields)

82943abbad2a8e46bef7b924c15a82a6dd0510ac
ollyfuse <cyotero26@gmail.com>
2025-09-01 09:10:25 +0200

    added env.examples

--- umugandatech/settings.py
+++ umugandatech/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y$4=58&b=^z9n$^c(f6j0_^^3@9g)ub=z^=5z)o2^qbd13uw!_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

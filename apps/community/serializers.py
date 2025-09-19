da972f611b143af4d40490c457422d98601a9d17
ollyfuse <cyotero26@gmail.com>
2025-09-19 10:21:48 +0200

    feat: deployment add:- joining the project functionality -notification on leader -debug some error

--- apps/community/serializers.py
+++ apps/community/serializers.py
@@ -15,22 +15,28 @@ class CommentSerializer(serializers.ModelSerializer):
         return super().create(validated_data)
 
 class PostSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
     upvotes_count = serializers.ReadOnlyField()
     has_upvoted = serializers.SerializerMethodField()
-    comments = CommentSerializer(many=True, read_only=True)
+    user_name = serializers.SerializerMethodField()
     comments_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ["id", "user", "project", "content", "type", "upvotes_count", "has_upvoted", "comments", "comments_count", "created_at"]
-        read_only_fields = ["created_at"]
+        fields = [
+            'id', 'title', 'description', 'sector', 'datetime', 'location',
+            'content', 'type', 'user', 'user_name', 'project', 
+            'upvotes_count', 'has_upvoted', 'comments_count', 'created_at'
+        ]
+        read_only_fields = ['created_at', 'user']
 
     def get_has_upvoted(self, obj):
         request = self.context.get('request')
         if request and request.user.is_authenticated:
             return obj.has_upvoted(request.user)
         return False
+    
+    def get_user_name(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}".strip() or obj.user.phone_number
     
     def get_comments_count(self, obj):
         return obj.comments.count()
